bb89e1971c2fc3a98ae7c8e727a8a7f6520eac5f
pesti8983 <pratiktry3@gmail.com>
2025-11-12 18:54:55 +0530

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,8 @@
+ #include <iostream>
+#include "MathLibrary.h"
+
+int main() {
+    std::cout << "Add: " << add(4, 3) << std::endl;
+    std::cout << "Subtract: " << subtract(4, 3) << std::endl;
+    return 0;
+}
